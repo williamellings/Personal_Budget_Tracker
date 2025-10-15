e810b9ce9451cce77f7b6fa130928ff5ae05b6ef
williamellings <wille.ellings@hotmail.com>
2025-10-15 20:09:13 +0200

    La till fler metoder som removetransaction med index och la även till det i main, men även in i main så man kan kalla på dem.

--- BudgetManager.cs
+++ BudgetManager.cs
@@ -48,5 +48,36 @@ namespace Personal_Budget_Tracker
             }
             Console.WriteLine($"In the bank: {InTheBank} kr");
         }
+
+        public void RemoveTransaction()
+        {
+            if (transactions.Count == 0)
+            {
+                Console.WriteLine("Inga transaktioner att ta bort.");
+                return;
+            }
+
+          
+            for (int i = 0; i < transactions.Count; i++)
+            {
+                Console.Write($"{i}: ");
+                transactions[i].ShowInfo();
+            }
+
+            Console.WriteLine("What trans do you want to remove? Choice index: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int index) && index >= 0 && index < transactions.Count)
+            {
+                InTheBank -= transactions[index].Amount;        // Uppdatera saldot
+                transactions.RemoveAt(index);            // Tar bort transaktionen
+                Console.WriteLine("Transaction removed.");
+            }
+            else
+            {
+                Console.WriteLine("Wrong index.");
+            }
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             bool start = true;
 
             BudgetManager budgetManager = new BudgetManager();
+            Transaction transaction = new Transaction();
 
             while (start)
             {
@@ -14,7 +15,7 @@
 
                 Console.WriteLine("1. Add transaction");
                 Console.WriteLine("2. Show all transactions");
-                Console.WriteLine("3. Show total balance");
+                Console.WriteLine("3. Show total balance and info");
                 Console.WriteLine("4. Remove an transaction");
                 Console.WriteLine("5. Quit program");
 
@@ -24,6 +25,27 @@
                 {
                     case "1":
                     budgetManager.AddTransaction();
+                        break;
+
+                    case "2":
+                        budgetManager.ShowAll();
+                        break;
+
+                    case "3":
+                        transaction.ShowInfo();
+                        break;
+
+                    case "4":
+                        budgetManager.RemoveTransaction();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Programmet avslutas");
+                        start = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong choice pick a number between (1-5)");
                         break;
                 }
 
